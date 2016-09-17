7b6350a4684541b3302a3e11d0ddbc490a0eba0e
Cooper Pellaton <c@cepp.at>
2016-09-17 16:26:44 -0400

    Now just serving html, not more pug view model shit.

--- app.js
+++ app.js
@@ -57,8 +57,7 @@ mongoose.connection.on('error', () => {
  * Express configuration.
  */
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(express.static('static'));
 app.use(compression());
 app.use(sass({
     src: path.join(__dirname, 'public'),
@@ -102,31 +101,7 @@ app.use(function(req, res, next) {
     }
     next();
 });
-app.use(express.static(path.join(__dirname, 'public'), {
-    maxAge: 31557600000
-}));
-/*
-MULTI PART FILE UPLOAD
-*/
-// tell express to use the bodyParser middleware                                                 
-// and set upload directory                                                                      
-app.use(express.bodyParser({
-    keepExtensions: true,
-    uploadDir: "uploads"
-}));
-app.engine('jade', require('jade').__express);
-app.post("/upload", function(request, response) {
-    // request.files will contain the uploaded file(s),                                          
-    // keyed by the input name (in this case, "file")                                            
-    // show the uploaded file name                                                               
-    console.log("file name", request.files.file.name);
-    console.log("file path", request.files.file.path);
-    response.end("upload complete");
-});
-// render file upload form                                                                       
-app.get("/", function(request, response) {
-    response.render("upload_form.jade");
-});
+
 /**
  * Primary app routes.
  */
