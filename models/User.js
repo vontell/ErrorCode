71e40a0210234024f524015cde28c33c6f007080
Cooper Pellaton <c@cepp.at>
2016-09-17 15:35:25 -0400

    I fixed the broken shit. My bad.

--- models/User.js
+++ models/User.js
@@ -51,16 +51,16 @@ userSchema.methods.comparePassword = function (candidatePassword, cb) {
   });
 };
 
-/**
- * Helper method for getting user's gravatar.
- */
-userSchema.methods.gravatar = function (size = 200) {
-  if (!this.email) {
-    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-  }
-  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
-  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
-};
+// /**
+//  * Helper method for getting user's gravatar.
+//  */
+// userSchema.methods.gravatar = function (size = 200) {
+//   if (!this.email) {
+//     return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+//   }
+//   const md5 = crypto.createHash('md5').update(this.email).digest('hex');
+//   return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+// };
 
 const User = mongoose.model('User', userSchema);
 
