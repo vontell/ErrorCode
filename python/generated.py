289dfcdb210db9a9d3df26414d59d1dd44f4a773
Cooper Pellaton <c@cepp.at>
2016-09-18 10:32:41 -0400

    Nested iteration of test challenges.

--- python/generated.py
+++ python/generated.py
@@ -1,32 +1,20 @@
 import unittest
 import logging
 my_logger = logging.getLogger('myapp')
-logging.basicConfig(filename='python/testresults.log', level=logging.INFO)
-
-
+logging.basicConfig(filename='python/testresults.log',level=logging.INFO)
 def function():
-    return 6
+    return 6takes as input a
+number x, and outputs that number squared.
+'''
+def square(x):
+    return x * x
 
-
+'''
+This function returns whether a given list
+is empty or not.
+'''
+def isEmpty(list):
+    numItems = len(list)
+    return numItems == 0
 class UserTestCase0(unittest.TestCase):
-
     def runTest(self):
-
-    def function():
-
-
-class UserTestCase1(unittest.TestCase):
-
-    def runTest(self):
-        return 6
-
-
-def suite():
-    test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.makeSuite(UserTestCase0))
-    test_suite.addTest(unittest.makeSuite(UserTestCase1))
-    return test_suite
-
-mySuite = suite()
-runner = unittest.TextTestRunner()
-my_logger.log(logging.INFO, runner.run(mySuite))
