1b4bdae133f9add7eeafe234c73ffa346d832a67
Aaron Vontell <vontell@mit.edu>
2016-09-18 10:45:13 -0400

    Assorted fixes.

--- python/code/exampleCode.py
+++ python/code/exampleCode.py
@@ -1,14 +1,10 @@
-'''
-This is a function that takes as input a
-number x, and outputs that number squared.
-'''
+# This is a function that takes as input a
+# number x, and outputs that number squared.
 def square(x):
     return x * x
 
-'''
-This function returns whether a given list
-is empty or not.
-'''
+# This function returns whether a given list
+# is empty or not.
 def isEmpty(list):
     numItems = len(list)
     return numItems == 0
